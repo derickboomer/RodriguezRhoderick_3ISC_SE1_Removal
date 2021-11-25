5f4bbc3373cb36b87e597bea2ac8231c86159104
Derick <rhoderick.rodriguez.cics@ust.edu.ph>
2021-11-25 13:50:46 +0800

    added script8.js

---
+++ js/script8.js
@@ -0,0 +1,13 @@
+class mySchool {
+    constructor(name, location, population){
+        this.name = name;
+        this.location = location;
+        this.population = population;
+    }
+}
+
+var course = new mySchool('UST', 'Manila', '40000');
+
+console.log(course);
+ 
+
