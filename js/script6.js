d9ed44d62e47651a1eb1c02c7d1c187f83b412d4
Derick <rhoderick.rodriguez.cics@ust.edu.ph>
2021-11-25 13:32:01 +0800

    added script6.js

---
+++ js/script6.js
@@ -0,0 +1,11 @@
+class Dog {
+    constructor(name, breed, color){
+        this.name = name;
+        this.breed = breed;
+        this.color = color;
+    }
+}
+
+var dogOne = new Dog('Derick', 'Filipino Bulldog', 'Pink');
+
+console.log(dogOne);
