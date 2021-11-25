c0154b59e67f7c45419be04e4ded8713deab9ba3
Derick <rhoderick.rodriguez.cics@ust.edu.ph>
2021-11-25 13:03:13 +0800

    added script1.js

---
+++ js/script1.js
@@ -0,0 +1,23 @@
+let tintin = {
+
+    breed: "Ragdoll",
+    color: "Calico",
+    age: 1,
+    gender: "female",
+    
+    speak:  function() {
+        console.log("Meow...")
+    },
+    eat: function(){
+        console.log("I love to eat tuna")
+    }
+
+   
+}
+
+tintin.speak();
+tintin.eat();
+   
+
+
+    
